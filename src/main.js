21757d418dcd61891427f6b72fc7125d8eb4a956
Scott Ellison <sellison@gmail.com>
2021-12-04 18:21:48 -0500

    nothing works :(

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ let createQuestionP = function(t) {
 
     return p
 }
+
 let createAnswerP = function(t) {
     let p = new docx.Paragraph({ 
         keepLines: true,       
@@ -25,6 +26,49 @@ let createAnswerP = function(t) {
 
     return p
 }
+
+
+let createTableP = function(dataRows) {
+
+    let t = new docx.Table({
+        columnWidths: [3505, 5505],
+        borders: {
+            top: {
+                style: docx.BorderStyle.DASH_DOT_STROKED,
+                size: 1,
+                color: "ff0000",
+            },
+            bottom: {
+                style: docx.BorderStyle.THICK_THIN_MEDIUM_GAP,
+                size: 5,
+                color: "889900",
+            },
+        },
+        rows: [new docx.TableRow({
+            children: [new docx.TableCell({
+                children: [new docx.Paragraph("test")]
+            })]
+        })]
+    })
+
+    dataRows.forEach(dr => {
+
+        let tr = new docx.TableRow({
+            children: [new docx.TableCell({
+                children: [new docx.Paragraph(dr.val)]
+            })]
+        })
+       // let tc = 
+        //let tp = new docx.Paragraph(dr.val)
+
+        //tc.children.push(tp)
+        //tr.children.push(tc)
+        
+    })
+
+    return t
+}
+
 let createOptionsP = function(t) {
     t = "Possible Options: " + t
     let p = new docx.Paragraph({ 
@@ -42,6 +86,7 @@ let createOptionsP = function(t) {
 
     return p
 }
+
 let createS = function(newLine, p) {
     let section = {
         properties: {
@@ -66,23 +111,69 @@ let createTrueS = function(name, paras) {
         ]
     })
 
-    paras.unshift(p)
+    if (Array.isArray(paras)) {
+        paras.unshift(p)
+    }
+    else {
+        paras = new Array(new docx.Table({
+        columnWidths: [3505, 5505],
+        borders: {
+            top: {
+                style: docx.BorderStyle.DASH_DOT_STROKED,
+                size: 1,
+                color: "ff0000",
+            },
+            bottom: {
+                style: docx.BorderStyle.THICK_THIN_MEDIUM_GAP,
+                size: 5,
+                color: "889900",
+            },
+        },
+        rows: [new docx.TableRow({
+                children: [new docx.TableCell({
+                 children: [new docx.Paragraph("test")]
+                })]
+            })]
+            })
+        )
+    }
 
     let section = {
         properties: {
             type: docx.SectionType.CONTINUOUS
         },
-        children: paras
+        children: [new docx.Table({
+            columnWidths: [3505, 5505],
+            borders: {
+                top: {
+                    style: docx.BorderStyle.DASH_DOT_STROKED,
+                    size: 1,
+                    color: "ff0000",
+                },
+                bottom: {
+                    style: docx.BorderStyle.THICK_THIN_MEDIUM_GAP,
+                    size: 5,
+                    color: "889900",
+                },
+            },
+            rows: [new docx.TableRow({
+                    children: [new docx.TableCell({
+                     children: [new docx.Paragraph("test")]
+                    })]
+                })]
+            })
+        ]
+            
     }    
     return section
 }
 
-//let questionChildren = []
 let sectionsList = []
 
 for(const section in data) {
 
         let paras = []
+        let tablePara = null
         let thisSection 
 
         if (Object.hasOwnProperty.call(data, section)) {
@@ -92,24 +183,41 @@ for(const section in data) {
             {
                 if (Object.hasOwnProperty.call(el.questions, q)) {
                     const qel = el.questions[q];
-                    let questionPara = createQuestionP(qel.text)
-                    //sectionsList.push(createS(true, questionPara))
-                    let answerPara = createAnswerP(qel.answer)
-                    //sectionsList.push(createS(false, answerPara))
-                    paras.push(questionPara, answerPara)
-                    if (qel.type === "dropdown") {
-                        paras.push(createOptionsP(qel.options))
+
+                    // let questionPara = createQuestionP(qel.text)
+                    // paras.push(questionPara)
+
+                    if (qel.type === "table") {
+                        let fakeDataRow = []
+                        fakeDataRow.push({val: "5.3"})
+                        fakeDataRow.push({val: "9.2"})
+                        tablePara = createTableP(fakeDataRow)                        
                     }
+                    // else {                    
+                    //     let answerPara = createAnswerP(qel.answer)
+                    //     paras.push(answerPara)
+
+                    //     if (qel.type === "dropdown") {
+                    //         paras.push(createOptionsP(qel.options))
+                    //     }
+                    // }
                 }
             }
 
-
             if (Object.hasOwnProperty.call(data, section)) {
                 const el = data[section];
-                thisSection = createTrueS(el.name, paras)
+
+                if (tablePara != null) {
+                    thisSection = createTrueS(el.name, tablePara)
+                 }
+                // else {
+                //     thisSection = createTrueS(el.name, paras)
+                // }
+                // tablePara = null
             }
 
-            sectionsList.push(thisSection)
+            if (thisSection)
+                sectionsList.push(thisSection)
     }
 }
 
