33bd13e34ba0d8d75fd0f6eb3b1ddfe0ec10211e
Scott Ellison <sellison@gmail.com>
2021-12-04 08:15:32 -0500

    initial add - basic doc created for questions and answers

---
+++ src/main.js
@@ -0,0 +1,105 @@
+const docx = require("docx")
+const fs = require("fs")
+const data = require("./data/data.json")
+
+let createQuestionP = function(t) {
+    let p = new docx.Paragraph({
+        heading: docx.HeadingLevel.HEADING_3,
+        spacing: {
+            before: 300
+        },
+        children: [
+            new docx.TextRun(t)
+        ]
+    })
+
+    return p
+}
+let createAnswerP = function(t) {
+    let p = new docx.Paragraph({ 
+        keepLines: true,       
+        children: [
+            new docx.TextRun(t)
+        ]
+    })
+
+    return p
+}
+let createS = function(newLine, p) {
+    let section = {
+        properties: {
+            type: docx.SectionType.CONTINUOUS
+        },
+        children: [ 
+            p
+        ]
+    }
+    
+    return section
+}
+
+let createTrueS = function(name, paras) {
+    let p = new docx.Paragraph({
+        heading: docx.HeadingLevel.HEADING_1,
+        spacing: {
+            before: 500
+        },
+        children: [
+            new docx.TextRun(name)
+        ]
+    })
+
+    paras.unshift(p)
+
+    let section = {
+        properties: {
+            type: docx.SectionType.CONTINUOUS
+        },
+        children: paras
+    }    
+    return section
+}
+
+//let questionChildren = []
+let sectionsList = []
+
+for(const section in data) {
+
+        let paras = []
+        let thisSection 
+
+        if (Object.hasOwnProperty.call(data, section)) {
+            const el = data[section];
+
+            for(const q in el.questions)
+            {
+                if (Object.hasOwnProperty.call(el.questions, q)) {
+                    const qel = el.questions[q];
+                    let questionPara = createQuestionP(qel.text)
+                    //sectionsList.push(createS(true, questionPara))
+                    let answerPara = createAnswerP(qel.answer)
+                    //sectionsList.push(createS(false, answerPara))
+                    paras.push(questionPara, answerPara)
+                }
+            }
+
+            if (Object.hasOwnProperty.call(data, section)) {
+                const el = data[section];
+                thisSection = createTrueS(el.name, paras)
+            }
+
+            sectionsList.push(thisSection)
+    }
+}
+
+
+const doc = new docx.Document({
+    sections: sectionsList
+});
+
+// Used to export the file into a .docx file
+docx.Packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync("My Document.docx", buffer);
+});
+
+// Done! A file called 'My Document.docx' will be in your file system.
